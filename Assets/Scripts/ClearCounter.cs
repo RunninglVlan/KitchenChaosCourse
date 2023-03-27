cf72067668a1466b2425ee877d743fb465b46e13
RunninglVlan <RedCapShortCut@gmail.com>
2023-03-27 18:53:44 +0300

    refactor

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,9 +1,9 @@
 public class ClearCounter : Counter {
     public override void Interact(Player player) {
-        if (player.HasKitchenObject()) {
-            player.GetKitchenObject().Parent = this;
-        } else if (HasKitchenObject()) {
-            GetKitchenObject().Parent = player;
+        if (player.TryGetKitchenObject(out var playerObject)) {
+            playerObject.Parent = this;
+        } else if (TryGetKitchenObject(out var counterObject)) {
+            counterObject.Parent = player;
         }
     }
 }
--- Assets/Scripts/KitchenObjectParent.cs
+++ Assets/Scripts/KitchenObjectParent.cs
@@ -4,8 +4,16 @@ public abstract class KitchenObjectParent : MonoBehaviour {
     private KitchenObject? kitchenObject;
     public abstract Transform ObjectLocation { get; }
 
+    public bool TryGetKitchenObject(out KitchenObject foundObject) {
+        foundObject = null!;
+        if (kitchenObject == null) {
+            return false;
+        }
+        foundObject = kitchenObject;
+        return true;
+    }
+
     public void ClearKitchenObject() => kitchenObject = null;
-    public KitchenObject GetKitchenObject() => kitchenObject!;
     public void SetKitchenObject(KitchenObject value) => kitchenObject = value;
     public bool HasKitchenObject() => kitchenObject != null;
 }
