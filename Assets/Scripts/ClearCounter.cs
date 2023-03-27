6c2b69047ce51c57c530abd1218e208651e99d7d
RunninglVlan <RedCapShortCut@gmail.com>
2023-03-27 19:23:42 +0300

    fix

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,8 +1,12 @@
 public class ClearCounter : Counter {
     public override void Interact(Player player) {
+        var counterHasObject = TryGetKitchenObject(out var counterObject);
         if (player.TryGetKitchenObject(out var playerObject)) {
+            if (counterHasObject) {
+                return;
+            }
             playerObject.Parent = this;
-        } else if (TryGetKitchenObject(out var counterObject)) {
+        } else if (counterHasObject) {
             counterObject.Parent = player;
         }
     }
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -5,6 +5,9 @@ public class ContainerCounter : Counter {
     [SerializeField] private ContainerCounterVisual visual = null!;
 
     public override void Interact(Player player) {
+        if (player.HasKitchenObject()) {
+            return;
+        }
         var instance = Instantiate(kitchenObjectScriptable.prefab);
         instance.Parent = player;
         visual.Open();
