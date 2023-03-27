6886307930280a6f0f930c5296d69151aa1f0017
RunninglVlan <RedCapShortCut@gmail.com>
2023-03-27 17:09:56 +0300

    feat: setting counter on kitchen object when interacting with counter
    KitchenObject then clears previous counter's kitchenObject
    - Sets itself on new counter
    - Changes parent

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,19 @@ public class ClearCounter : MonoBehaviour {
     [SerializeField] private KitchenObjectScriptable kitchenObjectScriptable = null!;
     [SerializeField] private Transform top = null!;
 
+    private KitchenObject? kitchenObject;
+    public Transform Top => top;
+
+    public void ClearKitchenObject() => kitchenObject = null;
+    public void SetKitchenObject(KitchenObject value) => kitchenObject = value;
+    public bool HasKitchenObject() => kitchenObject != null;
+
     public void Interact() {
-        var kitchenObject = Instantiate(kitchenObjectScriptable.prefab, top);
-        Debug.Log(kitchenObject.Scriptable.objectName);
+        if (kitchenObject == null) {
+            var instance = Instantiate(kitchenObjectScriptable.prefab);
+            instance.ClearCounter = this;
+        } else {
+            Debug.Log(kitchenObject.ClearCounter);
+        }
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -4,4 +4,22 @@ public class KitchenObject : MonoBehaviour {
     [SerializeField] private KitchenObjectScriptable scriptable = null!;
 
     public KitchenObjectScriptable Scriptable => scriptable;
+    private ClearCounter? clearCounter;
+
+    public ClearCounter? ClearCounter {
+        get => clearCounter;
+        set {
+            if (clearCounter != null) {
+                clearCounter.ClearKitchenObject();
+            }
+            clearCounter = value!;
+            if (clearCounter.HasKitchenObject()) {
+                Debug.LogError($"Counter already has a {nameof(KitchenObject)}!");
+            }
+            clearCounter.SetKitchenObject(this);
+            var objectTransform = transform;
+            objectTransform.parent = clearCounter.Top;
+            objectTransform.localPosition = Vector3.zero;
+        }
+    }
 }
