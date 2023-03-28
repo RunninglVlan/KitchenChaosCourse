acaa8ea439cbea071a2a938b8c8c423ec35592b4
RunninglVlan <RedCapShortCut@gmail.com>
2023-03-28 12:44:40 +0300

    feat: added plate counter and plate prefab with scriptable

---
+++ Assets/Scripts/Counters/PlateCounter.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace Counters {
+    public class PlateCounter : Counter {
+        private const float MAX_SPAWN_TIME = 4;
+        private const float MAX_SPAWNED_PLATES = 4;
+
+        [SerializeField] private KitchenObjectScriptable plate = null!;
+        [SerializeField] private PlateCounterVisual visual = null!;
+
+        private float spawnTime;
+        private int spawnedPlates;
+
+        public override void Interact(Player player) {
+            if (spawnedPlates <= 0 || player.HasKitchenObject()) {
+                return;
+            }
+            KitchenObject.Spawn(plate, player);
+            visual.DestroyTop();
+            spawnedPlates--;
+        }
+
+        void Update() {
+            if (spawnedPlates >= MAX_SPAWNED_PLATES) {
+                return;
+            }
+            spawnTime += Time.deltaTime;
+            if (spawnTime < MAX_SPAWN_TIME) {
+                return;
+            }
+            spawnTime = 0;
+            spawnedPlates++;
+            visual.Spawn();
+        }
+    }
+}
