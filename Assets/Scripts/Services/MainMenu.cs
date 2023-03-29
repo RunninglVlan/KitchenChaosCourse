c0aab405350b84d5f1a229e6b6880b0c75bee94b
RunninglVlan <RedCapShortCut@gmail.com>
2023-03-29 20:43:58 +0300

    feat: added main menu and loading scenes

---
+++ Assets/Scripts/Services/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace Services {
+    public class MainMenu : UIService {
+        [NaughtyAttributes.Scene, SerializeField]
+        private string loadingScene = null!;
+
+        [NaughtyAttributes.Scene, SerializeField]
+        private string gameScene = null!;
+
+        void Start() {
+            document.rootVisualElement.Q<Button>("play").clicked += LoadGameScene;
+            document.rootVisualElement.Q<Button>("quit").clicked += Application.Quit;
+        }
+
+        private void LoadGameScene() {
+            SceneManager.LoadSceneAsync(loadingScene).completed += _ => {
+                SceneManager.LoadScene(gameScene);
+            };
+        }
+    }
+}
