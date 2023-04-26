9d674cc05af28d7329b693649b80fbe68d3519fd
RunninglVlan <RedCapShortCut@gmail.com>
2023-04-26 12:24:47 +0300

    refactor: extracted kitchen object scriptable to index conversion
    It will be used in stove counter for network too

--- Assets/Scripts/Services/KitchenObjectService.cs
+++ Assets/Scripts/Services/KitchenObjectService.cs
@@ -10,19 +10,22 @@ namespace KitchenChaos.Services {
         [SerializeField] private KitchenObjectScriptable[] scriptables = Array.Empty<KitchenObjectScriptable>();
 
         public void Spawn(KitchenObjectScriptable scriptable, IKitchenObjectParent parent) {
-            var scriptableIndex = Array.IndexOf(scriptables, scriptable);
+            var scriptableIndex = GetIndex(scriptable);
             Assert.IsTrue(scriptableIndex >= 0, $"{scriptable.name} is missing from the list");
             SpawnServerRpc(scriptableIndex, parent.NetworkObject);
         }
 
         [ServerRpc(RequireOwnership = false)]
         private void SpawnServerRpc(int scriptableIndex, NetworkObjectReference parentNetworkObjectReference) {
-            var scriptable = scriptables[scriptableIndex];
+            var scriptable = Get(scriptableIndex);
             var instance = Instantiate(scriptable.prefab);
             instance.GetComponent<NetworkObject>().Spawn();
             parentNetworkObjectReference.TryGet(out var parentNetworkObject);
             instance.Parent = parentNetworkObject.GetComponent<IKitchenObjectParent>();
         }
+
+        public int GetIndex(KitchenObjectScriptable scriptable) => Array.IndexOf(scriptables, scriptable);
+        public KitchenObjectScriptable Get(int scriptableIndex) => scriptables[scriptableIndex];
 
         public void Destroy(KitchenObject kitchenObject) {
             DestroyServerRpc(kitchenObject.NetworkObject);
