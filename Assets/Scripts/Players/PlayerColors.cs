2f9338a51f89a581a838ef7f9d9461b7a933f00b
RunninglVlan <RedCapShortCut@gmail.com>
2023-05-23 18:34:07 +0300

    refactor: using readonly list publicly

--- Assets/Scripts/Players/PlayerColors.cs
+++ Assets/Scripts/Players/PlayerColors.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace KitchenChaos.Players {
     [CreateAssetMenu(fileName = nameof(PlayerColors), menuName = "Scriptable/" + nameof(PlayerColors))]
     public class PlayerColors : ScriptableObject {
-        [SerializeField] private Color[] colors = Array.Empty<Color>();
+        [SerializeField] private List<Color> colors = null!;
 
-        public Color[] Get => colors;
+        public IReadOnlyList<Color> Get => colors.AsReadOnly();
     }
 }
